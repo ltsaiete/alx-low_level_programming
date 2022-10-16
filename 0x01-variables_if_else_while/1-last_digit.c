985a7ea9dcd30a33b3055e1ef1a9cfc3b79fddf3
Luis Saiete <luistsaiete@gmail.com>
2022-10-16 06:13:54 -0700

    fix task 2 to import stdio

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
-
+/**
+ * main - Entry point
+ * Description: 'Print a text using puts'
+ * Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
