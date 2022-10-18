146df33e1ab145c5dc8d957fe80a6239e2e04279
Luis Saiete <luistsaiete@gmail.com>
2022-10-18 16:47:05 -0700

    FIxx betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,21 @@
 
 int print_sign(int n)
 {
+	int r;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		r = 1;
 	} else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		r = 0;
 	} else
 	{
 		_putchar('-');
-		return (-1);
+		r = -1;
 	}
+
+	return (r);
 }
