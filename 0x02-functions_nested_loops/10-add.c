a5dbb3ce9fe4a1eaea23198885f20950bdf85c79
Luis Saiete <luistsaiete@gmail.com>
2022-10-19 07:36:51 -0700

    Add task 10 and 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @n1: : parameter
+ * @n2: parameter
+ * Description: 'Print a text using write'
+ * Return: Always 0 (Success)
+ */
+
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ * @n: parameter
+ * Description: 'Print a text using write'
+ * Return: Always 0 (Success)
+ */
+
+int print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	} else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	printf("98\n");
+}
