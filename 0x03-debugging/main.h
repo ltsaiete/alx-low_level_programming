e771fa338977dd8ce09e489f6c1b4d0913e4fe9f
Luis Saiete <luistsaiete@gmail.com>
2022-10-21 23:58:08 +0200

    Fix task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+// #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
