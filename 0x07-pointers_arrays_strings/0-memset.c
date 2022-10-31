8b560ac29df0aaf143b3cc6632e9565f06f226ac
Luis Saiete <luistsaiete@gmail.com>
2022-10-31 22:10:35 +0200

    Fix variables in functions 0 and 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,7 +11,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int counter = 0;
+	unsigned int counter = 0;
 
 	while (counter < n)
 	{
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
