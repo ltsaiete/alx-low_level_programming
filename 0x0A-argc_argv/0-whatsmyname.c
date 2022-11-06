bd65b15a41fc566cd448416ada03e8439cb38d16
Luis Saiete <luistsaiete@gmail.com>
2022-11-06 16:55:56 +0200

    Add function 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Args count
+ * @argv: Args vector
+ *
+ * Return: success
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	exit(EXIT_SUCCESS);
+}
