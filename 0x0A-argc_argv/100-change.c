5c0162ffafef16fed7b1095b3af58972d0e4ac1f
Luis Saiete <luistsaiete@gmail.com>
2022-11-06 22:05:31 +0200

    Add advanced task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _min - Entry point
+ *
+ * @a: Args count
+ * @b: Args vector
+ *
+ * Return: success
+ */
+
+int _min(int a, int b)
+{
+	return (a < b ? a : b);
+}
+
+/**
+ * changes - Entry point
+ *
+ * @coins: Args count
+ * @n: Args vector
+ *
+ * Return: success
+ */
+
+int changes(int coins[], int n)
+{
+	int i, minimum;
+
+	if (n == 0)
+		return (0);
+	for (i = 0; i < 5; i++)
+	{
+		if (n - coins[i] >= 0)
+		{
+			minimum = _min(coins[i], n);
+			return (1 + changes(coins, n - minimum));
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * @argc: Args count
+ * @argv: Args vector
+ *
+ * Return: success
+ */
+
+int main(int argc, char **argv)
+{
+	int number, ch;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+
+	number = atoi(argv[1]);
+
+	if (number < 0)
+	{
+		puts("0");
+		exit(EXIT_FAILURE);
+	}
+
+	ch = changes(coins, number);
+	printf("%d\n", ch);
+}
