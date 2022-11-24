5090cd5de9910d4903a2220cbc03826a7e07ff39
Luis Saiete <luistsaiete@gmail.com>
2022-11-25 00:18:09 +0200

    Fix function to free memory

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,10 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp;
-
-	while (head != NULL)
+	if (head != NULL)
 	{
-		temp = head;
-		head = head->next;
-		free(temp);
+		free(head);
+		free(head->str);
+		free_list(head->next);
 	}
 }
