b67d6a86fb493f402280f6316d0b499a3041b07f
Luis Saiete <luistsaiete@gmail.com>
2022-11-26 11:45:05 +0200

    Check number of allocated bytes

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -11,21 +11,10 @@
 int main(void)
 {
 	char *c;
-	int *i;
-	float *f;
-	double *d;
 
 	c = malloc_checked(sizeof(char) * 1024);
-	printf("%p\n", (void *)c);
-	i = malloc_checked(sizeof(int) * 402);
-	printf("%p\n", (void *)i);
-	f = malloc_checked(sizeof(float) * 100000000);
-	printf("%p\n", (void *)f);
-	d = malloc_checked(INT_MAX);
-	printf("%p\n", (void *)d);
+	printf("%lu\n", sizeof(c));
+	printf("%p\n", c);
 	free(c);
-	free(i);
-	free(f);
-	free(d);
 	return (0);
 }
--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,7 +11,7 @@ void *malloc_checked(unsigned int b)
 {
 	int *p;
 
-	p = malloc(sizeof(int) * b);
+	p = malloc(sizeof(b) * b);
 	if (p == NULL)
 		exit(98);
 
