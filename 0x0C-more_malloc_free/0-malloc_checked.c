d7f4c8bd784dea7785d58ff248be9c90a74ceb59
Luis Saiete <luistsaiete@gmail.com>
2022-11-26 11:30:45 +0200

    Add function to allocate memory to integers in project 0x0C

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked -  allocates memory using malloc.
+ * @b: amount of memory to allocate
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(sizeof(int) * b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef _main_h
+#define _main_h
+#include <stdlib.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif
