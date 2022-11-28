ad36ac764fddd2a65c14b4bd8f6824145ad62103
Luis Saiete <luistsaiete@gmail.com>
2022-11-28 16:55:06 +0200

    Add function to print elements of the list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: our listint_t list
+ *
+ * Return - size of the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	_putnumber(h->n);
+	_putchar('\n');
+	return 1 + print_listint(h->next);
+}
+
+/**
+ * _putnumber - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putnumber(int n)
+{
+	char buffer[15];
+	int LengthUsed = sprintf(buffer, "%d", n);
+	return (write(1, buffer, LengthUsed));
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef _lists_h
 #define _lists_h
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,5 +14,7 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+int _putchar(char c);
+int _putnumber(int n);
 size_t print_listint(const listint_t *h);
 #endif
