e23cfad74bf7597485f72ec117b95083b653d2cb
Luis Saiete <luistsaiete@gmail.com>
2022-11-30 23:58:05 +0200

    Fix function in 5 to free NULL list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
-
-	while (temp != NULL)
+	if (head != NULL)
 	{
-		temp = temp->next;
-		free(*head);
-		*head = temp;
+		listint_t *temp = *head;
+
+		while (temp != NULL)
+		{
+			temp = temp->next;
+			free(*head);
+			*head = temp;
+		}
 	}
 }
