372d61c8ce2d1fabd3c626cd5dc87c3a7da36ab6
Luis Saiete <luistsaiete@gmail.com>
2022-12-03 00:38:10 +0200

    Not use / and %

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_binary - prints the binary representation of a number.
@@ -9,24 +10,8 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 1)
-	{
-		_putchar('1');
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		print_binary(n / 2);
-		if (n % 2 == 0)
-		{
-			_putchar('0');
-		}
-		else
-		{
-			_putchar('1');
-		}
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+
+	printf("%lu", n & 1);
 }
