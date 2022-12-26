756808907bdab557e0dd510830202ab6aec65531
Luis Saiete <luistsaiete@gmail.com>
2022-12-26 12:02:22 +0200

    Fix month verifications

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,17 +8,18 @@
 
 int main(void)
 {
-	float n1 = 1, n2 = 1, next;
+	long double n1 = 1, n2 = 1, next;
+	long long n = 218922995834555169026;
 	int i;
 
 	for (i = 0; i < 97; i++)
 	{
-		printf("%.0f, ", n2);
+		printf("%.0Lf, ", n2);
 		next = n1 + n2;
 		n1 = n2;
 		n2 = next;
 	}
-	printf("%.0f\n", n2);
+	printf("%.0Lf\n", n2);
 
 	return (0);
 }
