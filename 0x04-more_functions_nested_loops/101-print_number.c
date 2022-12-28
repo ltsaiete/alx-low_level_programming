d00c95e84fa088b0b138eec8758729130e0d1629
Luis Saiete <luistsaiete@gmail.com>
2022-12-28 21:10:44 +0200

    Print number using built-in functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,32 @@
 #include <math.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+ * _pow - Calculates the power of x raised by y
+ * @x: base
+ * @y: exponent
+ *
+ * Return: void
+ */
+
+int _pow(int x, int y)
+{
+	if (y == 0)
+		return (1);
+
+	return (x * _pow(x, y - 1));
+}
+/**
+ * _log10 - Calculates the logarithm of a number in base 10
+ * @n: the number
+ *
+ * Return: void
+ */
+
+int _log10(int n)
+{
+	return ((n > 9) ? 1 + _log10(n / 10) : 0);
+}
 
 /**
  * print_number - prints a number
@@ -16,7 +42,8 @@ void print_number(int n)
 	}
 	else
 	{
-		digits = log10(abs(n));
+		digits = _log10(abs(n));
+
 		if (n < 0)
 		{
 			_putchar('-');
@@ -25,10 +52,11 @@ void print_number(int n)
 
 		for (i = digits; i >= 0; i--)
 		{
-			power = pow(10, i);
+			power = _pow(10, i);
 			div = abs(n / power);
 			n -= power * div;
 			_putchar(div + '0');
 		}
 	}
+	_putchar('\n');
 }
