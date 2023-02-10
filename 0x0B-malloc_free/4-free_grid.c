888535ed690288970d14443c9bee80cab23658c3
Luis Saiete <luistsaiete@gmail.com>
2023-02-10 14:27:05 +0200

    Fix task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,7 +9,13 @@
  *
  */
 
-void free_grid(int **grid, int __attribute__((__unused__)) height)
+void free_grid(int **grid, int height)
 {
+	int i;
+	
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
 	free(grid);
 }
