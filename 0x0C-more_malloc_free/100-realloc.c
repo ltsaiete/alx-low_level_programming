5d890f63bf23e2c9d2726df6745fdc2c5776834b
Luis Saiete <luistsaiete@gmail.com>
2023-02-11 22:49:45 +0200

    Fix task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,22 +15,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
+	if (new_size == old_size)
+		return (ptr);
 
 	if (ptr == NULL)
-	{
-		return (malloc(new_size));
-	}
+		return malloc(new_size);
 
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
 
-	if (new_size <= old_size)
+	if (new_size < old_size)
 	{
 		memcpy(new_ptr, ptr, new_size);
 		free(ptr);
